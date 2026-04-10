801957396e1b133fac69c81b155908d47635a1a7
Investigacionmedicina <investigacionmedicina@usc.edu.co>
2026-04-10 11:13:44 -0500

    Simplify to static HTML

--- cttg-app.jsx
+++ cttg-app.jsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useState, useEffect, useRef } from 'react';
 import { Search, LogOut, Download, Plus, Edit2, Trash2, Filter, Eye, EyeOff } from 'lucide-react';
 
--- pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import CTTGApp from './cttg-app';
-
-export default function Home() {
-  return <CTTGApp />;
-}
